33d8fcdc05f344409762254c20809fad28434316
freetimezp <kraftukrnet@ukr.net>
2025-02-24 18:29:07 +0200

    02. usefull-things-034-slick-carousel. UI. Slider Carousel animation.

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+var slickPrimary = {
+    autoplay: true,
+    autoplaySpeed: 2400,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 1800,
+    cssEase: 'cubic-bezier(0.84, 0, 0.08, 0.99)',
+    asNavFor: '.text-slider',
+    centerMode: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+};
+
+var slickSecondary = {
+    autoplay: true,
+    autoplaySpeed: 2400,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1800,
+    cssEase: 'cubic-bezier(0.84, 0, 0.08, 0.99)',
+    asNavFor: '.image-slider',
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+};
+
+
+$('.image-slider').slick(slickPrimary);
+$('.text-slider').slick(slickSecondary);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
